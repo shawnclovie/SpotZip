07d3028276d827375fdef5b8d245f8574bc8e1b3
Shawn Clovie <lightpnr@gmail.com>
2019-08-18 16:14:19 +0800

    change dependence Spot to 1.0.0

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
             targets: ["SpotZipArchive"]),
     ],
     dependencies: [
-		.package(url: "https://github.com/shawnclovie/Spot", .branch("master")),
+		.package(url: "https://github.com/shawnclovie/Spot", from: "1.0.0"),
     ],
     targets: [
         .target(
